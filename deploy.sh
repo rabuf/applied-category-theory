1b37db7ddc9221b3a06f931324eba743f04ced3b
Jared Summers <jtsummers@gmail.com>
2018-04-14 02:21:25 -0400

    Updated deploy again, fixing ssh-agent

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,16 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 echo $rev
 echo $REPO
 echo $SSH_REPO
-#ssh-add $KEY_FILE
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add $KEY_FILE
 
 gitbook install && gitbook build
 cd _book
 git init
 git remote add upstream $SSH_REPO
-git fetch upstream -key ../$KEY_FILE && git reset upstream/gh-pages
+git fetch upstream  && git reset upstream/gh-pages
 rm .travis.yml deploy.sh deploy_key.enc
 git add .
 git commit -m "Rebuild pages at ${rev}"
-git push upstream gh-pages -key ../$KEY_FILE
+git push upstream gh-pages
