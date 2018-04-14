d53619a0932ecfebd2f2b69b53e40b6b9c988358
Jared Summers <jtsummers@gmail.com>
2018-04-14 02:42:15 -0400

    Have deploy switch to right branch

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,7 @@ rev=$(git rev-parse --short HEAD)
 
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-echo $rev
-echo $REPO
-echo $SSH_REPO
+
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add $KEY_FILE
@@ -18,7 +16,9 @@ cd _book
 git init
 git remote add upstream $SSH_REPO
 git fetch upstream  && git reset upstream/gh-pages
+git checkout gh-pages
 rm .travis.yml deploy.sh deploy_key.enc
 git add .
 git commit -m "Rebuild pages at ${rev}"
-git push upstream gh-pages
+ls *
+#git push upstream gh-pages
